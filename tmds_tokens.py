fac16abfd9319189cd6d3151dab608481f95e19d
Tim 'mithro' Ansell <mithro@mithis.com>
2016-05-21 17:24:53 +1000

    Make the tokens singletons.

--- tmds_tokens.py
+++ tmds_tokens.py
@@ -10,8 +10,97 @@ TMDS Tokens
 
 from bit_utils import *
 
+def grouper(iterable, n, fill=None):
+    """
+    >>> list(grouper([1,2,3,4,5], 2))
+    [(1, 2), (3, 4), (5,)]
+    >>> list(grouper([1,2,3,4,5], 2, 'x'))
+    [(1, 2), (3, 4), (5, 'x')]
+    >>> list(grouper([1,2,3,4,5], 3))
+    [(1, 2, 3), (4, 5)]
+    >>> list(grouper([1,2,3,4,5], 3, 'x'))
+    [(1, 2, 3), (4, 5, 'x')]
+    >>> list(grouper([1,2,3,4], 3, 'x'))
+    [(1, 2, 3), (4, 'x', 'x')]
+    >>> list(grouper([1,2,3,4], 3, 0))
+    [(1, 2, 3), (4, 0, 0)]
+    """
+    out = []
+    for i in iterable:
+        out.append(i)
+        if len(out) == n:
+            yield tuple(out)
+            out = []
+
+    if out:
+        if fill is not None:
+            while len(out) < n:
+                out.append(fill)
+        yield tuple(out)
+
 
 class BitSequence(tuple):
+    """
+    >>> a8 = BitSequence(0)
+    >>> a8
+    (0, 0, 0, 0, 0, 0, 0, 0)
+    >>> int(a8)
+    0
+    >>> a8.__bin__()
+    '0b00000000'
+    >>> a8.__hex__()
+    '0x00'
+
+    >>> a10 = BitSequence(0, n=10)
+    >>> a10
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    >>> int(a10)
+    0
+    >>> a10.__bin__()
+    '0b0000000000'
+    >>> a10.__hex__()
+    '0x000'
+
+    >>> b8 = BitSequence(1)
+    >>> b8
+    (1, 0, 0, 0, 0, 0, 0, 0)
+    >>> int(b8)
+    1
+    >>> b8.__bin__()
+    '0b00000001'
+    >>> b8.__hex__()
+    '0x01'
+
+    >>> b10 = BitSequence(1, n=10)
+    >>> b10
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    >>> int(b10)
+    1
+    >>> b10.__bin__()
+    '0b0000000001'
+    >>> b10.__hex__()
+    '0x001'
+
+    >>> c8 = BitSequence(128)
+    >>> c8
+    (0, 0, 0, 0, 0, 0, 0, 1)
+    >>> int(c8)
+    128
+    >>> c8.__bin__()
+    '0b10000000'
+    >>> c8.__hex__()
+    '0x80'
+
+    >>> c10 = BitSequence(256, n=10)
+    >>> c10
+    (0, 0, 0, 0, 0, 0, 0, 0, 1, 0)
+    >>> int(c10)
+    256
+    >>> c10.__bin__()
+    '0b0100000000'
+    >>> c10.__hex__()
+    '0x100'
+    """
     # FIXME: Finish this...
     def __new__(cls, *args, n=8):
         if len(args) == 1 and isinstance(args[0], (list, tuple)):
@@ -19,12 +108,12 @@ class BitSequence(tuple):
         else:
             i = args[0]
             assert isinstance(i, int)
-            assert i > 0
+            assert i >= 0
             assert i < 2**n
 
             args = bits(i, n)
 
-        assert len(args) == n
+        assert len(args) == n, (len(args), n)
 
         o = tuple.__new__(cls, args)
         o.value = bint(args)
@@ -35,7 +124,11 @@ class BitSequence(tuple):
         return self.value
 
     def __bin__(self):
-        return "0b"+"".join(reversed(str(b) in self))
+        return "0b"+"".join(str(b) for b in reversed(self))
+
+    def __hex__(self):
+        nibbles = [BitSequence(g, n=4) for g in list(grouper(self, 4, fill=0))]
+        return "0x"+"".join(hex(int(n))[2:] for n in reversed(nibbles))
 
 # --
 
@@ -46,6 +139,8 @@ class TMDSToken(BitSequence):
     >>> assert t1.w == (0,0,0,0,0,0,0,0)
     >>> assert t1.x == 0
     >>> assert t1.i == 0
+    >>> t1a = TMDSToken([0,0,0,0,0,0,0,0,0,0])
+    >>> assert t1 is t1a
     >>> t2 = TMDSToken([0,0,0,0,0,0,0,0,0,1])
     >>> assert t2.w == (0,0,0,0,0,0,0,0)
     >>> assert t2.x == 0
@@ -68,7 +163,22 @@ class TMDSToken(BitSequence):
     >>> assert t5 != t1
     >>> t6 = TMDSToken(0,0,0,0,0,0,0,0,0,0)
     >>> assert t1 == t6
+    >>> # Testing Invert
+    >>> t1 = TMDSToken(1,1,1,1,1,1,1,1,x=1,i=1)
+    >>> assert t1.w == (1,1,1,1,1,1,1,1)
+    >>> assert t1.x == 1
+    >>> assert t1.i == 1
+    >>> t2 = t1.invert()
+    >>> assert t2.w == (0,0,0,0,0,0,0,0)
+    >>> assert t2.x == 1
+    >>> assert t2.i == 0
+
+    >>> TMDSToken._encoding_mapping.clear()
+    >>> TMDSToken._encoding_mapping
+    {}
     """
+
+    _encoding_mapping = {}
 
     def __new__(cls, b0, b1=None, b2=None, b3=None, b4=None, b5=None, b6=None, b7=None, x=None, i=None):
         if b1 is None:
@@ -85,10 +195,23 @@ class TMDSToken(BitSequence):
             assert len(b0) == 10, len(b0)
             b0, b1, b2, b3, b4, b5, b6, b7, x, i = b0
 
-        return BitSequence.__new__(cls, [b0, b1, b2, b3, b4, b5, b6, b7, x, i], n=10)
+        seq = (b0, b1, b2, b3, b4, b5, b6, b7, x, i)
+        if seq not in cls._encoding_mapping:
+            cls._encoding_mapping[seq] = BitSequence.__new__(cls, seq, n=10)
+
+        obj = cls._encoding_mapping[seq]
+        assert isinstance(obj, cls), (cls, seq, obj, cls._encoding_mapping)
+        return obj
 
     def extra(self):
         return {}
+
+    @classmethod
+    def rmapping(cls, bits):
+        assert len(bits) == 10
+        obj = TMDSToken(bits)
+        assert isinstance(obj, cls)
+        return obj
 
     @property
     def w(self):
@@ -116,7 +239,7 @@ class TMDSToken(BitSequence):
         return self.i == 0
 
     def __repr__(self):
-        return "{}({}, **{})".format(self.__class__.__name__, tuple(self), self.extra())
+        return "{}({}, {})".format(self.__class__.__name__, tuple(self), ", ".join("{}={}".format(k, v) for k,v in self.extra().items()))
  
     def __str__(self):
         n = self.__class__.__name__
@@ -128,33 +251,41 @@ class TMDSToken(BitSequence):
     def invert(self):
         """Invert the TMDS Token to the alternative.
 
-        >>> t1 = TMDSToken(1,1,1,1,1,1,1,1,x=1,i=1)
-        >>> assert t1.w == (1,1,1,1,1,1,1,1)
-        >>> assert t1.x == 1
-        >>> assert t1.i == 1
-        >>> t2 = t1.invert()
-        >>> assert t2.w == (0,0,0,0,0,0,0,0)
-        >>> assert t2.x == 1
-        >>> assert t2.i == 0
         """
         return self.__class__(inv(self[:-2])+[self.x]+inv([self.i]), **self.extra())
 
 # --
 
 class ControlToken(TMDSToken):
+    """
+    >>> gen = ControlToken.tokens()
+    >>> token1 = next(gen)
+    >>> assert ControlToken([0,0,1,0,1,0,1,0,1,1], c0=0, c1=0) is token1
+    >>> assert ControlToken.mapping(token1.c0, token1.c1) is token1
+    >>> assert ControlToken.rmapping((0,0,1,0,1,0,1,0,1,1)) is token1
+    >>> token2 = next(gen)
+    >>> assert token1 != token2
+    """
+
+    _control_mapping = {}
 
     def __new__(cls, *args, c0=None, c1=None):
-        return TMDSToken.__new__(cls, *args)
+        t = TMDSToken.__new__(cls, *args)
 
-    def __init__(self, *args, c0=None, c1=None):
         assert c0 in (0, 1)
         assert c1 in (0, 1)
+        t.c0 = c0
+        t.c1 = c1
 
-        self.c0 = c0
-        self.c1 = c1
+        cls._control_mapping[(c0, c1)] = t
+        return t
 
     def extra(self):
         return dict(c0=self.c0, c1=self.c1)
+
+    @classmethod
+    def mapping(cls, c0, c1):
+        return cls._control_mapping[(c0, c1)]
 
     @classmethod
     def tokens(cls):
@@ -166,18 +297,42 @@ class ControlToken(TMDSToken):
 # --
 
 class DataToken(TMDSToken):
+    """
+    >>> # Register all the tokens
+    >>> for t in DataToken.tokens():
+    ...     pass
+    >>> # Get them from the mappings
+    >>> DataToken.mapping(0x10)
+    {DataToken((0, 0, 0, 0, 1, 1, 1, 1, 1, 0), data=0x10)}
+    >>> DataToken.mapping(0x00)
+    {DataToken((1, 1, 1, 1, 1, 1, 1, 1, 1, 1), data=0x0), DataToken((0, 0, 0, 0, 0, 0, 0, 0, 1, 0), data=0x0)}
+    >>> DataToken.rmapping((0, 0, 0, 0, 1, 1, 1, 1, 1, 0))
+    DataToken((0, 0, 0, 0, 1, 1, 1, 1, 1, 0), data=0x10)
+    >>> DataToken.rmapping((1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    DataToken((1, 1, 1, 1, 1, 1, 1, 1, 1, 1), data=0x0)
+    """
+
+    _data_mapping = {}
 
     def __new__(cls, *args, data=None):
-        return TMDSToken.__new__(cls, *args)
+        t = TMDSToken.__new__(cls, *args)
 
-    def __init__(self, *args, data=None):
         assert isinstance(data, int)
         assert data >= 0
         assert data < 256
-        self.data = data
+        t.data = data
+
+        if data not in cls._data_mapping:
+            cls._data_mapping[data] = set()
+
+        cls._data_mapping[data].add(t)
+        return t
 
     def extra(self):
         return dict(data=self.data)
+
+    def __repr__(self):
+        return "{}({}, data={})".format(self.__class__.__name__, tuple(self), hex(self.data))
 
     @classmethod
     def tokens(cls):
@@ -186,6 +341,10 @@ class DataToken(TMDSToken):
         for i in range(0, 256):
             for token in cls.generate_tokens(i):
                 yield token
+
+    @classmethod
+    def mapping(cls, data):
+        return cls._data_mapping[data]
 
     # --------------------------------------------------------------------
 
@@ -311,7 +470,7 @@ if __name__ == "__main__":
         assert transitions(data_token.w) <= 4, data_token
 
     # Check there are no duplicate tokens
-    tokens = list(ControlTokens.tokens())+list(DataTokens.tokens())
+    tokens = list(ControlToken.tokens())+list(DataToken.tokens())
     for t1 in tokens:
         for t2 in tokens:
             if t1 is t2:
